ff87e9b8ec67080fae2a9ff66087f6ce9f295034
Franco <francoezequielmassaccesi@gmail.com>
2025-06-20 18:14:03 -0300

    criando a contagem regresiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('hola mundo');
+AOS.init();
+
+const dataDoEvento = new Date('Jun 30, 2025 19:00:00');
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento -timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60
+    const segundoEmMs = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteoEvento = Math.floor((distanciaAteOEvento % (diasEmMs)) / (horaEmMs));
+    const minutosAteoEvento = Math.floor((distanciaAteOEvento % (horaEmMs)) / (minutoEmMs));
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % (minutoEmMs)) / (segundoEmMs));
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteoEvento}h ${minutosAteoEvento}m ${segundosAteOEvento}s`;
+
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000)
